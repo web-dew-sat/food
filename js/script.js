9cbab5bf4965aa0e9b6a8cd807d0364d8e0d207a
Stas <stastimofeew98@gmail.com>
2024-01-05 23:14:22 +0300

    Получение данных с сервера, заполение данных в карточке товара с сервера

--- js/script.js
+++ js/script.js
@@ -189,36 +189,22 @@ window.addEventListener('DOMContentLoaded', () => {
             this.parent.append(element);
         }
     }
+    const getResource = async (url) => {
+        const res = await fetch(url);
 
-    new MenuCard(
-        'img/tabs/vegy.jpg',
-        'vegy',
-        'Меню "Фитнес"',
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-        9,
-        '.menu .container'
-    ).render();
+        if(!res.ok){
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
 
-    new MenuCard(
-        'img/tabs/elite.jpg',
-        'elite',
-        'Меню “Премиум”',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        24,
-        '.menu .container',
-        'menu__item'
-    ).render();
-    
-    new MenuCard(
-        'img/tabs/post.jpg',
-        'post', 
-        'Меню "Постное"',
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-        13,
-        '.menu .container',
-        'menu__item'
-    ).render();
-    
+        return await res.json();
+    };
+
+    getResource('http://localhost:3000/menu')
+    .then(data => {
+        data.forEach(({img, altimg, title, descr, price}) => {
+            new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+        });
+    });
 
     // Forms
 
@@ -231,10 +217,22 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     forms.forEach(item => {
-        postData(item);
+        bindPostData(item);
     });
+
+    const postData = async (url, data) => {
+        const res = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: data
+        });
+
+        return await res.json();
+    };
     
-    function postData(form){
+    function bindPostData(form){
         form.addEventListener('submit', (e) => {
             e.preventDefault();
 
@@ -249,21 +247,9 @@ window.addEventListener('DOMContentLoaded', () => {
             
             const formData = new FormData(form);
 
-            const object = {};
+            const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-            formData.forEach(function(value, key){
-                object[key] = value;
-            })
-
-           
-            fetch('server.php', {
-                method: 'POST',
-                headers: {
-                    'Content-type': 'application/json'
-                },
-                body: JSON.stringify(object)
-            })
-            .then(data => data.text())
+            postData('http://localhost:3000/requests', json)
             .then(data => {
                 console.log(data);
                 showThanksModal(message.success);
@@ -304,7 +290,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     }    
 
-    fetch('http://localhost:3000/menu')
-    .then(data => data.json())
-    .then(res => console.log(res));
+    // fetch('http://localhost:3000/menu')
+    // .then(data => data.json())
+    // .then(res => console.log(res));
 });
