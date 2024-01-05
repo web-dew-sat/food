d68726f298ab79b43ce87ec7d152ca9bf055ddd0
Stas <stastimofeew98@gmail.com>
2024-01-05 22:22:56 +0300

    Добавление npm пакета: json-server

--- js/script.js
+++ js/script.js
@@ -303,4 +303,8 @@ window.addEventListener('DOMContentLoaded', () => {
         },4000)
 
     }    
+
+    fetch('http://localhost:3000/menu')
+    .then(data => data.json())
+    .then(res => console.log(res));
 });
